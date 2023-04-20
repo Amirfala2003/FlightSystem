ec80c20fca92e79c6104a9be6a3675945e86c4d6
Amirfala2003 <131403392+Amirfala2003@users.noreply.github.com>
2023-04-21 00:14:52 +0330

    Added project to github from local

---
+++ class16.java
@@ -0,0 +1,72 @@
+
+import java.util.Scanner;
+
+public class class16 {
+    public static void main(String[] args) {
+        //Scanner input = new Scanner(System.in);
+        Menu menu = new Menu();
+
+
+    }
+
+    //private static void Print(String Text) {
+    //  System.out.println(Text);
+    //}
+
+    //private static void Error(String ErrorText) {
+    //    System.err.println(ErrorText);
+    //}
+
+
+//    private static void ShowMatrix(int[][] matrix) {
+//        System.out.println("|-------|-------|-------|-------|");
+//        for (int row = 0; row < 4; row++) {
+//            for (int column = 0; column < 4; column++) {
+//                if (column == 0) {
+//                    System.out.print("|" + "\t");
+//
+//                }
+//                if (matrix[row][column] == -1) {
+//                    System.err.print("#");
+//                    System.out.print("\t"+"|"+"\t");
+//                } else if (matrix[row][column] == -2) {
+//                    System.out.print("O" + "\t"+"|"+"\t");
+//
+//                } else if (matrix[row][column] == -3) {
+//                    System.out.print("X" + "\t"+"|"+"\t");
+//                } else {
+//                    System.out.print(matrix[row][column] + "\t" + "|" + "\t");
+//                }
+//            }
+//
+//            System.out.println();
+//            System.out.println("|-------|-------|-------|-------|");
+//
+//
+//        }
+//
+//
+//    }
+//    private static void ShowMatrix(int[][] matrix) {
+//        for (int row = 0; row < 4; row++) {
+//            for (int column = 0; column < 4; column++) {
+//                if (matrix[row][column] == -1) {
+//                    System.out.print("#" + "\t");
+//                } else if (matrix[row][column] == -2) {
+//                    System.out.print("O" + "\t");
+//
+//                } else if (matrix[row][column] == -3) {
+//                    System.out.print("X" + "\t");
+//
+//                } else {
+//                    System.out.print(matrix[row][column] + "\t");
+//                }
+//
+//            }
+//            System.out.println();
+//
+//
+//        }
+//
+//    }
+}
